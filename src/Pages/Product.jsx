707d6affc7b841eb524324248030fea84371ad2a
JanithKE <49825461+JanithKE@users.noreply.github.com>
2023-11-01 04:45:05 +0530

    update

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import {ShopContext} from '../Context/ShopContext.jsx';
+import BreadCrums from '../Components/BreadCrums/BreadCrums.jsx';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay.jsx';
+
+
 
 const Product = () => {
+
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+
+  const product = all_product.find((e) => e.id === Number(productId));
+
   return (
     <div>
-      
+      <BreadCrums product={product}/>
+      <ProductDisplay product={product}/>
     </div>
   )
 }
